7f3665a4643601ba7045297c593eccf3e49e2833
Marvin Affeldt <45835050+affeldt28@users.noreply.github.com>
2023-07-04 16:56:49 +0200

    feat: add prettier

---
+++ .prettierrc.cjs
@@ -0,0 +1,29 @@
+/** @type {import('@types/prettier').Config} */
+module.exports = {
+	semi: true,
+	singleQuote: true,
+	quoteProps: 'as-needed',
+	jsxSingleQuote: false,
+	trailingComma: 'all',
+	bracketSpacing: true,
+	bracketSameLine: false,
+	arrowParens: 'always',
+	proseWrap: 'preserve',
+	htmlWhitespaceSensitivity: 'css',
+	vueIndentScriptAndStyle: false,
+	embeddedLanguageFormatting: 'auto',
+	singleAttributePerLine: false,
+
+	plugins: [
+		'@trivago/prettier-plugin-sort-imports',
+		'prettier-plugin-tailwindcss',
+		'prettier-plugin-astro',
+	],
+
+	// @trivago/prettier-plugin-sort-imports
+	importOrder: ['^@(.*)$', '^~/(.*)$', '^[./]'],
+	importOrderSeparation: true,
+
+	// prettier-plugin-tailwindcss
+	tailwindConfig: './tailwind.config.cjs',
+};
